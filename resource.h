8a3b7701bd38e7d0ccf664d53b4bd4bba80b4f8d
StayHydated <68977953+StayHydated@users.noreply.github.com>
2021-02-22 17:33:33 -0600

    Add files via upload

---
+++ resource.h
@@ -0,0 +1,47 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource Monitor.rc
+//
+#define IDC_MYICON                      2
+#define IDD_RESOURCEMONITOR_DIALOG      102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_RESOURCEMONITOR             107
+#define IDI_SMALL                       108
+#define IDC_RESOURCEMONITOR             109
+#define IDR_MAINFRAME                   128
+#define IDD_ADDDIALOG                   129
+#define IDD_EDITDIALOG                  130
+#define BTN_CPULOAD                     1000
+#define BTN_CPUFREQ                     1001
+#define BTN_MEMLOAD                     1002
+#define BTN_MEMINUSE                    1003
+#define BTN_DISKLOAD                    1004
+#define BTN_DISKSPEED                   1005
+#define BTN_DISKREAD                    1006
+#define BTN_DISKWRITE                   1007
+#define IDC_COMBOLABEL                  1009
+#define IDC_EDITTYPE                    1010
+#define IDC_COMBOFONT                   1011
+#define IDC_BTNCOLOR                    1019
+#define IDC_EDITFONTSIZE                1020
+#define IDC_COMBOBKG                    1023
+#define IDC_BUTTONDEL                   1024
+#define IDC_COMBOUNDERLINE              1025
+#define IDC_CHECKVIEW                   1026
+#define IDC_CHECKSTARTUP                1029
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1030
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
